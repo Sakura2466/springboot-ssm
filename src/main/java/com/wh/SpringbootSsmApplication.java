8de1949956c3587dd2b9f2660ced82147a209152
wanghao <774797233@qq.com>
2022-07-22 14:30:39 +0800

    变更

--- src/main/java/com/wh/SpringbootSsmApplication.java
+++ src/main/java/com/wh/SpringbootSsmApplication.java
@@ -3,10 +3,14 @@ package com.wh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Wang Hao
+ * @since 2022/7/18 16:54
+ */
 @SpringBootApplication
 public class SpringbootSsmApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         SpringApplication.run(SpringbootSsmApplication.class, args);
     }
 

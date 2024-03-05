e9ba7f73e975441c86a5ec77ad02f9403bfb491d
wh <774797233@qq.com>
2024-03-05 20:23:15 +0800

    springssm

--- src/main/java/com/wh/model/Book.java
+++ src/main/java/com/wh/model/Book.java
@@ -1,14 +1,21 @@
 package com.wh.model;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Wang Hao
  * @since 2022/7/18 11:49
  */
+@Repository
 @Data
 public class Book {
+    @Value("${book.id}")
     private Integer id;
+    @Value("${book.name}")
     private String name;
+    @Value("${book.type}")
     private String type;
 }
--- src/test/java/com/wh/SpringbootSsmApplicationTests.java
+++ src/test/java/com/wh/SpringbootSsmApplicationTests.java
@@ -1,14 +1,24 @@
 package com.wh;
 
+import com.wh.model.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class SpringbootSsmApplicationTests {
-
+     @Autowired
+     Book book;
+     @Value("#{book.id}")
+     String str;
     @Test
     void contextLoads() {
-
+        System.out.println(book);
+        System.out.println(str);
     }
 
 }
